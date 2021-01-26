5ab27758465661893fafb80875db1c6101b5ef8d
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-26 08:38:00 -0500

    commit tree 50bf92e4c89aec7c0b8938ffc6b441fe24309b94

--- demo.js
+++ demo.js
@@ -21,13 +21,13 @@ function likeCallback(e) {
   mimicServerCall()
     .then(function(serverMessage){
       // STEP 2: Uncomment the next 3 lines.
-       We'll use Pillar 1 (DOM Manipulation) to update the screen and
-       mimic Pillar 3 (Server Communication) to only update the screen if the
-       sending of information to the server succeeds.
+       //We'll use Pillar 1 (DOM Manipulation) to update the screen and
+       //mimic Pillar 3 (Server Communication) to only update the screen if the
+       //sending of information to the server succeeds.
       alert("You notified the server!");
-      // alert(serverMessage);
-      // heart.innerText = glyphStates[heart.innerText];
-      // heart.style.color = colorStates[heart.style.color];
+       alert(serverMessage);
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
     })
     .catch(function(error) {
       alert("Something went wrong!");
